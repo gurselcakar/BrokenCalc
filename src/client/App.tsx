8b1262c7b061687f32d9f986719b697e045a0272
gurselcakar <87243002+gurselcakar@users.noreply.github.com>
2025-06-28 17:58:09 +0100

    Start repository

---
+++ src/client/App.tsx
@@ -0,0 +1,11 @@
+import { BoltBadge } from './BoltBadge';
+import { Game } from './Game';
+
+export const App = () => {
+  return (
+    <>
+      <Game />
+      <BoltBadge mode="white" />
+    </>
+  );
+};
